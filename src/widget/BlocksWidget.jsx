318a264d146c2474097e1167b9c089e7bde1a6cf
Giulia Ghisini <giulia.ghisini@redturtle.it>
2021-01-12 16:58:34 +0100

    fixed bug on save

--- src/widget/BlocksWidget.jsx
+++ src/widget/BlocksWidget.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 //import { defineMessages, useIntl } from 'react-intl';
+import { v4 as uuid } from 'uuid';
+import { isEmpty } from 'lodash';
 import { Form as UIForm } from 'semantic-ui-react';
 import { FormFieldWrapper, Form } from '@plone/volto/components';
-
+import { settings } from '~/config';
+import './blocks_widget.css';
 //const messages = defineMessages({
 // footerItemsHeader: {
 //   id: 'editablefooter-items-header',
@@ -13,9 +16,22 @@ import { FormFieldWrapper, Form } from '@plone/volto/components';
 const BlocksWidget = (props) => {
   const { value, id, onChange, required } = props;
   //const intl = useIntl();
+  const defaultBlockId = uuid();
+  if (!value.blocks_layout || isEmpty(value.blocks_layout.items)) {
+    value.blocks_layout = {
+      items: [defaultBlockId],
+    };
+  }
+  if (!value.blocks || isEmpty(value.blocks)) {
+    value.blocks = {
+      [defaultBlockId]: {
+        '@type': settings.defaultBlockType,
+      },
+    };
+  }
 
   const onChangeBlocks = (data) => {
-    onChange({
+    onChange(id, {
       blocks: data.blocks,
       blocks_layout: data.blocks_layout,
     });
@@ -25,8 +41,7 @@ const BlocksWidget = (props) => {
     <div className="blocks-widget">
       <UIForm.Field inline id={id}>
         <FormFieldWrapper {...props}>
-          blocks widget
-          <div className="menu-blocks-container">
+          <div className="blocks-widget-container">
             <Form
               key={id}
               formData={value}
